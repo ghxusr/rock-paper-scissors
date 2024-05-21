b2ef9f527460f25a7337812d9d58aeb4acd7918b
ghxusr <nldevsol@protonmail.com>
2024-05-21 17:00:30 +0200

    Add getHumanChoice() function with logic to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,23 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "Rock";
+        return "rock";
     }
 
     else if (choice === 1) {
-        return "Paper";
+        return "paper";
     }
 
-    else return "Scissors";
+    else return "scissors";
 }
 
+function getHumanChoice() {
+    let input = (prompt("Please enter your choice between rock paper scissors")).toLowerCase();
+
+    do {
+        input = (prompt("Please enter your choice between rock paper scissors")).toLowerCase();
+    }
+    while ((input != "rock") && (input != "paper") && (input != "scissors"))
+    
+    return input;
+}
