ce03d5c1263cc4a3119968a0bbf7851fe0b6ee2d
ghxusr <nldevsol@protonmail.com>
2024-06-06 19:37:18 +0200

    Remove playGame() function and add on click event listeners to buttons to play a round

--- javascript.js
+++ javascript.js
@@ -21,69 +21,79 @@ function getHumanChoice() {
     return input;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        let result = "";
-        switch (humanChoice) {
-            case "rock":
-                if (computerChoice === "paper") {
-                    ++computerScore;
-                    result = "You Lose! Paper beats Rock";
-                    return result;
-                }
-                else if (computerChoice === "scissors") {
-                         ++humanScore;
-                         result = "You Win! Rock Beats Scissors";
-                         return result;
-                }
-                else if (computerChoice === "rock") {
-                    result = "You Draw! Both players chose Rock";
-                    return result;
-                }
-                break;
-                case "paper":
-                if (computerChoice === "rock") {
-                    ++humanScore;
-                    result = "You Win! Paper beats Rock";
-                    return result;
-                }
-                else if (computerChoice === "scissors") {
-                         ++computerScore;
-                         result = "You Lose! Scissors Beats Paper";
-                         return result;
-                }
-                else if (computerChoice === "paper") {
-                    result = "You Draw! Both players chose Paper";
-                    return result;
-                }
-                break;
-                case "scissors":
-                if (computerChoice === "paper") {
-                    ++humanScore;
-                    result = "You Win! Scissors beats Paper";
-                    return result;
-                }
-                else if (computerChoice === "rock") {
-                         ++computerScore;
-                         result = "You Lose! Rock Beats Scissors";
-                         return result;
-                }
-                else if (computerChoice === "scissors") {
-                         result = "You Draw! Both players chose Scissors";
-                         return result;
-                }
-        }
+function playRound(humanChoice, computerChoice) {
+    let result = "";
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "paper") {
+                ++computerScore;
+                result = "You Lose! Paper beats Rock";
+                return result;
+            }
+            else if (computerChoice === "scissors") {
+                     ++humanScore;
+                     result = "You Win! Rock Beats Scissors";
+                     return result;
+            }
+            else if (computerChoice === "rock") {
+                result = "You Draw! Both players chose Rock";
+                return result;
+            }
+            break;
+            case "paper":
+            if (computerChoice === "rock") {
+                ++humanScore;
+                result = "You Win! Paper beats Rock";
+                return result;
+            }
+            else if (computerChoice === "scissors") {
+                     ++computerScore;
+                     result = "You Lose! Scissors Beats Paper";
+                     return result;
+            }
+            else if (computerChoice === "paper") {
+                result = "You Draw! Both players chose Paper";
+                return result;
+            }
+            break;
+            case "scissors":
+            if (computerChoice === "paper") {
+                ++humanScore;
+                result = "You Win! Scissors beats Paper";
+                return result;
+            }
+            else if (computerChoice === "rock") {
+                     ++computerScore;
+                     result = "You Lose! Rock Beats Scissors";
+                     return result;
+            }
+            else if (computerChoice === "scissors") {
+                     result = "You Draw! Both players chose Scissors";
+                     return result;
+            }
     }
-
-    /*for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection) + "\nScore" + "\nYou : " + humanScore + "\nComputer : " + computerScore);
-    }*/
 }
 
-playGame();
+const rockButton = document.querySelector("#rock-button");
 
+rockButton.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    console.log(playRound("rock", computerSelection) + "\nScore" + "\nYou : " + humanScore + "\nComputer : " + computerScore);
+});
+
+const paperButton = document.querySelector("#paper-button");
+
+paperButton.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    console.log(playRound("paper", computerSelection) + "\nScore" + "\nYou : " + humanScore + "\nComputer : " + computerScore);
+});
+
+const scissorsButton = document.querySelector("#scissors-button");
+
+scissorsButton.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    console.log(playRound("scissors", computerSelection) + "\nScore" + "\nYou : " + humanScore + "\nComputer : " + computerScore);
+});
